5c7c7a6c1e0a885b5ae17af6714c260a606ce16f
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-04-27 19:34:22 -0400

    adding spdlog and premake stuff

---
+++ genproj.bat
@@ -0,0 +1,2 @@
+call thirdparty\bin\premake\premake5.exe vs2019
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,52 @@
+workspace "visions2D"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}.%{cfg.architecture}"
+
+IncludeDirectories = {}
+IncludeDirectories["spdlog"] = "%{prj.name}/thirdparty/spdlog"
+LibDirectories = {}
+
+project "visions2D"
+    location "visions2D"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("binobj/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{IncludeDirectories.spdlog}"
+    }
+
+    libdirs
+    {
+
+    }
+
+    links
+    {
+
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "visions2D_DEBUG"
+        }
