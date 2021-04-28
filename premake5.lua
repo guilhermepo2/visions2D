90e9e0ae791ca3b4ef94c235f5ebe480ff4491fc
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-04-28 14:37:26 -0400

    adding glew and sdl2

--- premake5.lua
+++ premake5.lua
@@ -9,8 +9,12 @@ workspace "visions2D"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}.%{cfg.architecture}"
 
 IncludeDirectories = {}
+IncludeDirectories["SDL2"] = "%{prj.name}/thirdparty/SDL2-2.0.12/include"
+IncludeDirectories["GLEW"] = "%{prj.name}/thirdparty/glew-2.1.0/include"
 IncludeDirectories["spdlog"] = "%{prj.name}/thirdparty/spdlog/include"
 LibDirectories = {}
+LibDirectories["SDL2"] = "%{prj.name}/thirdparty/SDL2-2.0.12/lib/x64"
+LibDirectories["GLEW"] = "%[prj.name]/thirdparty/glew-2.1.0/lib/Release/x64"
 
 project "visions2D"
     location "visions2D"
@@ -28,17 +32,22 @@ project "visions2D"
 
     includedirs
     {
-        "%{IncludeDirectories.spdlog}"
+        "%{IncludeDirectories.spdlog}",
+        "%{IncludeDirectories.SDL2}",
+        "%{IncludeDirectories.GLEW}"
     }
 
     libdirs
     {
-
+        "%{LibDirectories.SDL2}",
+        "%{LibDirectories.GLEW}",
     }
 
     links
     {
-
+        "SDL2.lib",
+        "glew32.lib",
+        "opengl32.lib"
     }
 
     filter "system:windows"
