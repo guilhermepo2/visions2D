1478e46d35ab9394d3f24654383b274fbac8cccd
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-07-03 21:30:01 -0400

    changes on premake files

--- premake5.lua
+++ premake5.lua
@@ -14,21 +14,6 @@ workspace "visions2D"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}.%{cfg.architecture}"
 
-IncludeDirectories = {}
-IncludeDirectories["SDL2"] = "visions2D/thirdparty/SDL2-2.0.12/include"
-IncludeDirectories["SDL2_TTF"] = "visions2D/thirdparty/SDL2_ttf-2.0.15/include"
-IncludeDirectories["GLEW"] = "visions2D/thirdparty/glew-2.1.0/include"
-IncludeDirectories["spdlog"] = "visions2D/thirdparty/spdlog/include"
-IncludeDirectories["DEARIMGUI"] = "visions2D/thirdparty/dearimgui"
-IncludeDirectories["STBIMAGE"] = "visions2D/thirdparty/stb"
-IncludeDirectories["GLM"] = "visions2D/thirdparty/glm"
-IncludeDirectories["RAPIDJSON"] = "visions2D/thirdparty/rapidjson-1.1.0/include"
-
-LibDirectories = {}
-LibDirectories["SDL2"] = "visions2D/thirdparty/SDL2-2.0.12/lib/x64"
-LibDirectories["GLEW"] = "visions2D/thirdparty/glew-2.1.0/lib/Release/x64"
-LibDirectories["SDL2_TTF"] = "visions2D/thirdparty/SDL2_ttf-2.0.15/lib/x64"
-
 include "v2d_standalone.lua"
 include "premake_sandbox.lua"
 
