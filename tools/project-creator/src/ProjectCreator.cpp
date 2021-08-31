46aeb4499872d47757adaf7f6d2e30f7daede608
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-08-31 13:28:51 -0400

    generating a game project file

--- tools/project-creator/src/ProjectCreator.cpp
+++ tools/project-creator/src/ProjectCreator.cpp
@@ -8,11 +8,25 @@
 
 #include "Log.h"
 #include "Utilities/JsonHelper.h"
+#include <fstream>
 
 enum class PROJECTTYPE {
 	NONE,
 	TOOL,
 	GAME
+};
+
+const static std::string Dependencies[] = {
+	"%{prj.name}/src",
+	"visions2D/src",
+	"%{IncludeDirectories.SDL2}",
+	"%{IncludeDirectories.SDL2_TTF}",
+	"%{IncludeDirectories.GLAD}",
+	"%{IncludeDirectories.spdlog}",
+	"%{IncludeDirectories.DEARIMGUI}",
+	"%{IncludeDirectories.STBIMAGE}",
+	"%{IncludeDirectories.GLM}",
+	"%{IncludeDirectories.RAPIDJSON}"
 };
 
 // ************************************************************************
@@ -32,10 +46,60 @@ void CreateToolFileProject(const std::string& Name, const std::string& Location)
 
 // ************************************************************************
 // ************************************************************************
+// TODO: This works, but there's probably a better way to do it.
+// 1. Have a template?
 void CreateGameFileProject(const std::string& Name, const std::string& Location) {
 	LOG_INFO("Creating GAME file");
 
-	// create file (premake_{Name}.lua)
+	// 1. create the file
+	std::string Filename = std::string("premake_") + Name + std::string(".lua");
+	std::ofstream File(Filename.c_str());
+	if (!File.is_open()) {
+		LOG_ERROR("couldn't create {0} file", Filename);
+	}
+
+	// ==================================================
+	// doing the dirt work
+	File << "project \"" + Name + "\"\n";
+	File << "\tlocation\"" + Location + "\"\n";
+	File << "\tkind \"ConsoleApp\"\n";
+	File << "\tlanguage \"C++\"\n";
+	File << "\twarnings \"Extra\"\n";
+	File << "\n";
+	File << "\ttargetdir(\"bin/\" ..outputdir .. \"/%{prj.name}\")\n";
+	File << "\tobjdir(\"binobj/\" ..outputdir .. \"/%{prj.name}\")\n";
+	File << "\n";
+	File << "\tfiles\n\t{\n";
+	File << "\t\t\"%{prj.name}/**.h\",\n";
+	File << "\t\t\"%{prj.name}/**.cpp\"\n";
+	File << "\t}";
+	File << "\n\n";
+	
+	File << "\tincludedirs\n\t{\n";
+	for(auto dependency : Dependencies) {
+		File << "\t\t\"" + dependency + "\"\n";
+	}
+	File << "\t}\n";
+
+	File << "\tfilter \"system:windows\"\n";
+	File << "\t\tcppdialect \"C++17\"\n";
+	File << "\t\tstaticruntime \"On\"\n";
+	File << "\t\tsystemversion \"latest\"\n";
+	File << "\n";
+	File << "\t\tdefines\n\t\t{\n";
+	File << "\t\t\t\"visions2D_DEBUG\",\n";
+	File << "\t\t\t\"sandbox_DEBUG\"\n";
+	File << "\t\t}\n";
+
+	// ==================================================
+	// 2. Open "premake5.lua"
+	std::ifstream PremakeFile("../../../premake5.lua");
+	if (!PremakeFile.is_open()) {
+		LOG_ERROR("Couldn't open premake5.lua file");
+	}
+
+
+	
 	// fill contents
 	// add it to "premake5.lua"
 }
@@ -44,6 +108,15 @@ void CreateGameFileProject(const std::string& Name, const std::string& Location)
 // ************************************************************************
 void CreateWithSettings(const std::string& ProjectName, const std::string& Location, PROJECTTYPE ProjectType) {
 	LOG_INFO("Creating ({0}, {1}, {2})", ProjectName, Location, ProjectType);
+
+	switch (ProjectType) {
+	case PROJECTTYPE::TOOL: {
+		CreateToolFileProject(ProjectName, Location);
+	} break;
+	case PROJECTTYPE::GAME: {
+		CreateGameFileProject(ProjectName, Location);
+	} break;
+	}
 }
 
 // ************************************************************************
@@ -77,6 +150,9 @@ int main(int argc, char* argv[]) {
 		LOG_INFO("arg {0}: {1}", i, argv[i]);
 	}
 
+	CreateFromFilename("test.json");
+
+	/*
 	// what is needed to create a project?
 	// project name
 	// location name
@@ -92,6 +168,7 @@ int main(int argc, char* argv[]) {
 	else {
 		LOG_WARNING("Please specify a filename or project name, location and type.");
 	}
+	*/
 
 	return 0;
 }
